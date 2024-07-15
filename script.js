9bb3b04c851ea6728235caf0557980197eeadcf1
Rayyan7878 <rayyanshaikh787898@gmail.com>
2024-07-15 09:09:19 -0700

    Done

--- script.js
+++ script.js
@@ -29,39 +29,11 @@ window.onscroll = () => {
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const navbar = document.querySelector('.header');
-// window.onscroll = () => {
-//     if (window.scrollY > 300) {
-//         navbar.classList.add('nav-active');
-//     } else {
-//         navbar.classList.remove('nav-active');
-//     }
-// };
+const navbar = document.querySelector('.header');
+window.onscroll = () => {
+    if (window.scrollY > 300) {
+        navbar.classList.add('nav-active');
+    } else {
+        navbar.classList.remove('nav-active');
+    }
+};
